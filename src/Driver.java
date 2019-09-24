391d0349d625eef744c1e2234e7707db519c8b55
Will Shields <43360999+wshields@users.noreply.github.com>
2019-09-24 16:27:30 -0400

    Working matrix input

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Driver {
 
@@ -15,6 +16,23 @@ public class Driver {
         n = keyboard.nextInt();
 
         f = new int[m+1][n+1];
+
+        System.out.println("Enter the values for the boolean array. These are done row by row and should be ones and zeroes only.");
+        for(int i = 0; i < m + 1; i++) {
+            for(int j = 0; j < n + 1; j++) {
+                int temp = keyboard.nextInt();
+                if(temp == 1 || temp == 0) {
+                    f[i][j] = temp;
+                }else {
+                    System.out.println("Bad Input. Try again");
+                    j--;
+                }
+            }
+        }
+
+        for(int i = 0; i < m + 1; i++) {
+            System.out.println(Arrays.toString(f[i]));
+        }
 
     }
 
