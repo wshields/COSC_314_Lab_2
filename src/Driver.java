46548df9a94d5ee34d20fe88a01f591d3c442601
Will Shields <43360999+wshields@users.noreply.github.com>
2019-09-24 23:25:48 -0400

    Adding function tests to meet minimum requirements

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Driver {
 
-    public static void main(String args[]){
-        int m , n;
-        int [][] f;
+    public static void main(String args[]) {
+        int m, n;
+        int[][] f;
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -15,24 +14,24 @@ public class Driver {
         System.out.println("Enter second integer:");
         n = keyboard.nextInt();
 
-        f = new int[m+1][n+1];
+        f = new int[m + 1][n + 1];
 
         System.out.println("Enter the values for the boolean array. These are done row by row and should be ones and zeroes only.");
-        for(int i = 0; i < m + 1; i++) {
-            for(int j = 0; j < n + 1; j++) {
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
                 int temp = keyboard.nextInt();
-                if(temp == 1 || temp == 0) {
+                if (temp == 1 || temp == 0) {
                     f[i][j] = temp;
-                }else {
+                } else {
                     System.out.println("Bad Input. Try again");
                     j--;
                 }
             }
         }
 
-        for(int i = 0; i < m + 1; i++) {
-            System.out.println(Arrays.toString(f[i]));
-        }
+        Function func = new Function(m, n, f);
+        func.testFunction();
+
 
     }
 
--- src/Function.java
+++ src/Function.java
@@ -1,9 +1,68 @@
 public class Function {
 
+    private int rows;
+    private int columns;
+    private int[][] functionImages;
 
 
-    public Function(int m , int n , int[][] f){
+    // Constructor for function
+    public Function(int m, int n, int[][] f) {
+        this.rows = m + 1;
+        this.columns = n + 1;
+        this.functionImages = f;
+    }
 
+    public void testFunction() {
+        this.oneToOne(this.functionImages);
+        this.onTo(this.functionImages);
+    }
+
+    private void oneToOne(int[][] f) {
+        boolean oneToOne = false;
+        for(int i = 0; i < this.columns; i++) {
+            int oneCount = 0;
+            for(int j = 0; j < this.rows; j++) {
+                if(f[j][i] == 1) {
+                    oneCount++;
+                }
+            }
+
+            if(oneCount == 1) {
+                oneToOne = true;
+            } else if(oneCount > 1) {
+                oneToOne = false;
+                break;
+            }
+        }
+        if (oneToOne) {
+            System.out.println("Function is one-to-one");
+        } else {
+            System.out.println("Function is NOT one-to-one");
+        }
+    }
+
+    private void onTo(int[][] f) {
+        boolean onTo = false;
+        for(int i = 0; i < this.columns; i++) {
+            int oneCount = 0;
+            for(int j = 0; j < this.rows; j++) {
+                if(f[j][i] == 1) {
+                    oneCount++;
+                }
+            }
+
+            if(oneCount >= 1) {
+                onTo = true;
+            } else {
+                onTo = false;
+                break;
+            }
+        }
+        if (onTo) {
+            System.out.println("Function is on to");
+        } else {
+            System.out.println("Function is NOT on to");
+        }
     }
 
 }
